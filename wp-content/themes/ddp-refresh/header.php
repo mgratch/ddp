dcf12ce8874196200876ce51c66ff2088dd20723
Jeff Winegar <jeff@evolveinsideout.com>
2015-10-21 10:42:39 -0400

    made fb sdk only load on homepage

--- wp-content/themes/ddp-refresh/header.php
+++ wp-content/themes/ddp-refresh/header.php
@@ -38,15 +38,18 @@
 			wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<!-- Facebook SDK -->
-		<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1662450680668324";
-		fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));</script>
+
+		<?php if (is_front_page()): ?>
+			<!-- Facebook SDK -->
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1662450680668324";
+			fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
+		<?php endif ?>
 
 		<div class="content-wrap">
 			<header class="header header--main js-main-header">
